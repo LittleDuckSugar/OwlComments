429744e509375af399601caa50515c53f5bbb1a6
BERNARD Antoine <bernard_antoine@outlook.com>
2022-03-22 22:32:38 +0100

    Remove db init in main

--- API/main.go
+++ API/main.go
@@ -1,25 +1,10 @@
 package main
 
 import (
-	"context"
 	"owlcomments/controller"
-	"owlcomments/repository"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-func init() {
-	// Init the db connection
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(repository.UriDB))
-
-	if err != nil {
-		panic(err)
-	}
-
-	repository.Client = client
-}
 
 func main() {
 	// Tells to gin if we are in a dev environment or not
